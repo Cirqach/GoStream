4b7b00245bfb44af6fd6898d70d08ae8f13d3db5
Cirqach <unamed@duck.com>
2024-09-01 13:48:03 +0300

    add color to logs

--- cmd/logger/logger.go
+++ cmd/logger/logger.go
@@ -6,16 +6,28 @@ import (
 	"strings"
 )
 
+var (
+	colors = map[string]string{
+		"red":     "\033[31m",
+		"green":   "\033[32m",
+		"cyan":    "\033[36m",
+		"reset":   "\033[0m",
+		"yellow":  "\033[33m",
+		"gray":    "\033[37m",
+		"magenta": "\033[35m",
+	}
+)
+
 // LogError function    logging error message
 func LogError(funcName, message string) {
 	funcName = cleanFuncName(funcName)
-	log.Println("ERROR in func " + funcName + " : " + message)
+	log.Println(colors["red"] + "ERROR" + colors["reset"] + " in func " + colors["magenta"] + funcName + colors["reset"] + " : " + colors["gray"] + message + colors["reset"])
 }
 
 // LogMessage function    logging information message
 func LogMessage(funcName, message string) {
 	funcName = cleanFuncName(funcName)
-	log.Println("MESSAGE from " + funcName + " : " + message)
+	log.Println(colors["cyan"] + "MESSAGE" + colors["reset"] + " from " + colors["magenta"] + funcName + colors["reset"] + " : " + colors["grey"] + message + colors["reset"])
 }
 
 // GetFuncName function    return name of function where it called
@@ -33,7 +45,7 @@ func GetFuncName(skip int) string {
 
 func Fatal(funcName, message string) {
 	funcName = cleanFuncName(funcName)
-	log.Fatal("FATAL: in func " + funcName + " : " + message)
+	log.Fatal(colors["red"] + "FATAL: in func " + funcName + " : " + message + colors["reset"])
 }
 
 func cleanFuncName(funcName string) string {
